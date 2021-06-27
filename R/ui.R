1dd12f0eb8fb33c5a708eded9949d73024247e39
unknown <keruiduo@gmail.com>
2021-06-27 23:57:31 +0800

    Hop la

--- R/ui.R
+++ R/ui.R
@@ -3,12 +3,11 @@
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(shiny.i18n)
+
 i18n <- Translator$new(translation_json_path = "translation.json")
 i18n$set_translation_language("en")
 
@@ -21,18 +20,18 @@ shinyUI(fluidPage(
                    choices = i18n$get_languages(),
                    selected = i18n$get_key_translation())
   ),
-  
+
   titlePanel(i18n$t("*** Lotka Volterra Predator-Prey relations ***")),
-  
+
   sidebarLayout(
     sidebarPanel(
       tabPanel(title="Lotka Volterra Predator-Prey",
                helpText(h3(i18n$t("Set parameters for the L-V predator-prey model"))),
-               
+
                helpText(h4(i18n$t("Set starting population sizes"))),
                numericInput("N",label=p(i18n$t("Select a value for N (starting population of prey)")),value=25,min=1,max=50),
                numericInput("P",label=p(i18n$t("Select a value for P (starting population of predator)")),value=10,min=1,max=25),
-               
+
                helpText(h4(i18n$t("Prey carrying capacity"))),
                radioButtons("Prey_K", label="",
                             choices = list("No" = 1,
@@ -40,21 +39,21 @@ shinyUI(fluidPage(
                             selected = 1),
                # If users select "Prey carrying capacity" above, then generate the input option
                htmlOutput("UIpreyk"),
-               
+
                helpText(h4(i18n$t("Set demographic parameters"))),
-               sliderInput("r", 
+               sliderInput("r",
                            label = i18n$t("Choose a value for r (prey intrinsic growth rate)"),
                            min = 0.01, max = 1.99, value=1.0, step = NULL),
-               sliderInput("a", 
+               sliderInput("a",
                            label = i18n$t("Choose a value for a (predation efficiency)"),
                            min = .0001, max = .5, value=.1, step = NULL),
-               sliderInput("d", 
+               sliderInput("d",
                            label = i18n$t("Choose a value for d (predator death rate)"),
                            min = 0.01, max = 1, value=0.6, step = NULL),
-               sliderInput("b", 
+               sliderInput("b",
                            label = i18n$t("Choose a value for b (conversion efficiency)"),
                            min = 0.01, max = 1, value=.5, step = NULL),
-               
+
                numericInput("time",label=i18n$t("Number of time steps to run the model"),value = 100, min=1),
                actionButton("goButton", i18n$t("Go!"))
       )
@@ -81,7 +80,7 @@ shinyUI(fluidPage(
                  p("This changes the equilibrium isocline for the prey:"),
                  p(withMathJax("$$ \\frac{dN}{dt}=0 \\text{  when  } P = \\frac{r}{a}-\\frac{rP}{aK} $$"))
         ),
-        
+
         tabPanel(title = i18n$t("Plots"),
                  h4(i18n$t("Here are plots showing changes in populations")),
                  fluidRow(
@@ -93,7 +92,7 @@ shinyUI(fluidPage(
                             resetOnNew = TRUE
                           )))),
                  fluidRow(plotOutput("plot3"))
-                 
+
         ),
         tabPanel(title = "Credit",
                  h4("Developed under the supervision of Dr. Christophe Coupe, The University of Hong Kong")
